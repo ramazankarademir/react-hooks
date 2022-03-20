bb1bb9a312dcb563219b5adccad97aacbc9304c0
Ramazan Karademir <karademir.ramazan@gmail.com>
2022-03-20 19:01:01 +0300

    implemented custom hooks with localstorage usage

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,15 @@
 
 import * as React from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
-
-  function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
-  }
+function Greeting({initialName = ''}) {
+  const [name, setName] = React.useState(initialName)
+  const handleChange = event => setName(event.target.value)
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
